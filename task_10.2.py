a627be4485a82f3dc24b205a3aafeca4ecfe6454
mishutka200101 <79220116+mishutka200101@users.noreply.github.com>
2021-03-17 15:56:12 +0300

    Create task_10.2.py

---
+++ task_10.2.py
@@ -0,0 +1,23 @@
+llst = ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х',
+        'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
+blst = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х',
+        'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я']
+
+
+def encrypt_caesar(msg, shift):
+    ret = ""
+    for x in msg:
+        if x in llst:
+            ind = llst.index(x) % len(llst)
+            ret += llst[(ind + shift) % len(llst)]
+        elif x in blst:
+            ind = blst.index(x) % len(llst)
+            ret += blst[(ind + shift) % len(llst)]
+        else:
+            ret += x
+    return ret
+
+
+number = int(input())
+message = input()
+print(encrypt_caesar(message, number))
