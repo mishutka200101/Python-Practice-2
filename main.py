d44f40ce5a64b472322424b094c2d39a3a62fb9b
mishutka200101 <79220116+mishutka200101@users.noreply.github.com>
2021-03-17 12:52:31 +0300

    Add files via upload
    Task 9.2

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-f = int(input())
-s = int(input())
-first_books = []
-second_books = []
-for i in range(0, f):
-    first_books.insert(i, input())
-for i in range(0, s):
-    second_books.insert(i, input())
-for i in range(0, s):
-    if second_books[i] in first_books:
-        print('YES')
-    else:
-        print('NO')
+# test
