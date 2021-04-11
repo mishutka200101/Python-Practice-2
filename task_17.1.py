e37567e9be1f33cf46a4ab90979f429cfb35ef79
mishutka200101 <79220116+mishutka200101@users.noreply.github.com>
2021-04-11 23:04:13 +0300

    Create task_17.1.py

---
+++ task_17.1.py
@@ -0,0 +1,4 @@
+text = input()
+cyrillic = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+latin = 'a|b|v|g|d|e|e|zh|z|i|i|k|l|m|n|o|p|r|s|t|u|f|kh|tc|ch|sh|shch||y||e|iu|ia|A|B|V|G|D|E|E|Zh|Z|I|I|K|L|M|N|O|P|R|S|T|U|F|Kh|Tc|Ch|Sh|Shch||Y||E|Iu|Ia'.split('|')    
+print(text.translate({ord(k):v for k,v in zip(cyrillic,latin)}))
